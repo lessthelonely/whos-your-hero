4b197b9a48fdf9079ea1068097f21d7ee6e8351f
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2023-10-25 12:10:58 +0100

    OK! Starting to populate instances.

--- ontology/instance.py
+++ ontology/instance.py
@@ -1,6 +1,17 @@
 from onto import Character 
+import os
 
 character = Character("Cassandra Cain")
 character.isWoman.append(True)
 
-print(character)
+
+def load_aliases(names):
+    for folder_name in names:
+        f = open(folder_name + "/" + folder_name + "_Aliases.txt", "r")
+        for line in f:
+            if (line.strip() == ""):
+                continue
+            character.hasAlias.append(line.strip())
+
+load_aliases(["cassandra_cain"])
+
