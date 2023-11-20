f62c348fed5ffc0f874bea35e65ae77c6c2672a9
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2023-11-20 14:26:42 +0000

    Finished About View.

--- frontend/wsdl-wyh/src/views/AboutView.vue
+++ frontend/wsdl-wyh/src/views/AboutView.vue
@@ -1,18 +1,34 @@
 <template>
-  <div>
-    <b>Who's Your Hero?</b> is an ontology-based project, designed to help you find all you wish to know about your comic book heroes.
-    It covers a bunch of different characteristics, but more importantly it covers specific tropes that the character went through.
+  <div style="text-align: justify;">
+    <p>
+    <h3>What is this?</h3>
+    <b>Who's Your Hero?</b> is an ontology-based project, designed to help you find all you wish to know about your
+      comic book heroes.It covers a bunch of different characteristics, but more importantly it covers specific tropes that the
+      character wentthrough.
+    </p>
 
+    <br />
+
+    <p>
     <h3>What is a trope?</h3>
-    <quot>
-      A trope is a storytelling device or convention, a shortcut for describing situations the storyteller can reasonably assume the audience will recognize. Tropes are the means by which a story is told by anyone who has a story to tell. 
-    </quot>
-    
+    <blockquote class="blockquote" cite="https://tvtropes.org/pmwiki/pmwiki.php/Main/Tropes">
+      <p style="font-size: 16px; font-style: oblique; margin-bottom: 20px;">A trope is a storytelling device or convention, a shortcut for describing situations the storyteller can reasonably
+        assume the audience will recognize. Tropes are the means by which a story is told by anyone who has a story to tell.</p>
+      <footer class="blockquote-footer" style="font-size: 13px; justify-content: right; display: flex;">definition of trope, by <cite title="Source Title">TVTropes</cite></footer>
+    </blockquote>
+
+    </p>
+
+
   </div>
 </template>
 
 <style>
 b {
   font-weight: bold;
+}
+
+br {
+  margin: 3.25px;
 }
 </style>
