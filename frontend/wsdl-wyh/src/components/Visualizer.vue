31aac27801a8509155dc94117ef751ef062fbf5b
henriquecscode <henriquecostasousa.hcs@gmail.com>
2023-11-25 15:34:11 +0000

    Graph for tropes associated with characteres
    Co-authored-by: melisilva <melisilva@users.noreply.github.com>
    Co-authored-by: lessthelonely <lessthelonely@users.noreply.github.com>

--- frontend/wsdl-wyh/src/components/Visualizer.vue
+++ frontend/wsdl-wyh/src/components/Visualizer.vue
@@ -8,7 +8,8 @@
 <script>
 import { defineComponent, ref, onMounted } from 'vue'
 import cytoscape from 'cytoscape'
-import { Character } from '../stores/Character'
+import popper from 'cytoscape-popper'
+//import { Character } from '../stores/Character'
 
 
 export default defineComponent({
@@ -26,16 +27,124 @@ export default defineComponent({
     },
 
     props: {
-        character: Character
+        //character: Character,
+        tropeData: Object
     },
     watch: {
+        tropeData: function (val) {
+            console.log("tropedata", this.tropeData);
+            let characters = [];
+            let tropes = [];
+            let characterTropeList = [];
+            for (const [key, val] of Object.entries(this.tropeData)) {
+                tropes.push(key);
+                let belongsTo = val.belongsTo;
+                let tropeDescriptions = val.tropeDescription;
+                for (let i = 0; i < belongsTo.length; i++) {
+                    let character = belongsTo[i];
+                    let tropeCharacterDescription = tropeDescriptions[i];
+                    characters.push(character);
+                    characterTropeList.push({
+                        character: character,
+                        trope: key,
+                        description: tropeCharacterDescription
+                    });
+                }
+            }
+
+            // Make into a set
+            characters = [...new Set(characters)]
+
+            let tropeNodes = [];
+            let characterNodes = [];
+            let tropeEdges = [];
+
+            for (let i = 0; i < tropes.length; i++) {
+                tropeNodes.push({
+                    data: {
+                        id: tropes[i]
+                    },
+                    classes: ['trope']
+                });
+            }
+
+            for (let i = 0; i < characters.length; i++) {
+                characterNodes.push({
+                    data: {
+                        id: characters[i]
+                    },
+                    classes: ['character']
+                });
+            }
+
+            for (let i = 0; i < characterTropeList.length; i++) {
+                tropeEdges.push({
+                    data: {
+                        id: `${characterTropeList[i].character}-${characterTropeList[i].trope}`,
+                        source: characterTropeList[i].character,
+                        target: characterTropeList[i].trope
+                        // label: characterTropeList[i].description
+                    }
+                });
+            }
+
+            let cy = cytoscape({
+
+                container: this.cy, // container to render in
+                
+                elements: {// list of graph elements to start with
+                    nodes: [
+                        ...characterNodes,
+                        ...tropeNodes
+                    ],
+                    edges: [
+                        ...tropeEdges
+                    ]
+                },
+
+                style: [ // the stylesheet for the graph
+                    {
+                        selector: 'node',
+                        style: {
+                            'background-color': '#666',
+                            'label': 'data(id)'
+                        }
+                    },
+
+                    {
+                        selector: 'edge',
+                        style: {
+                            'width': 3,
+                            'line-color': '#ccc',
+                            'target-arrow-color': '#ccc',
+                            'target-arrow-shape': 'triangle',
+                            'curve-style': 'bezier'
+                        }
+                    }
+                ],
+
+                layout: {
+                    name: 'cose',
+                    nodeRepulsion: node => {
+                        if (node.hasClass('character')) {
+                            console.log("Node is character")
+                            return 1000000
+                        }
+                        console.log("Node is trope")
+                        return 1000
+                    }
+                    // rows: 1
+                }
+            })
+        }
+        /*
         character: function (val) {
             this.state = 'character changed'
 
             let characterName = this.character.name
-        let thisNode = {
-            data: {
-                    id:characterName,
+            let thisNode = {
+                data: {
+                    id: characterName,
                 },
                 classes: ['character']
             }
@@ -52,53 +161,53 @@ export default defineComponent({
                     classes: ['trope']
                 })
                 tropeEdges.push({
-                data: {
+                    data: {
                         id: `${this.character.name}-${key}`,
                         source: characterName,
                         target: key
-                }
+                    }
                 })
             }
 
             tropeNodes = tropeNodes.splice(0, 5);
             tropeEdges = tropeEdges.splice(0, 5);
 
-        let cy = cytoscape({
+            let cy = cytoscape({
 
                 container: this.cy, // container to render in
 
                 elements: {// list of graph elements to start with
                     nodes: [
-                thisNode,
+                        thisNode,
                         ...tropeNodes
-            ],
+                    ],
                     edges: [
                         ...tropeEdges
                     ]
                 },
 
-            style: [ // the stylesheet for the graph
-                {
-                    selector: 'node',
-                    style: {
-                        'background-color': '#666',
-                        'label': 'data(id)'
-                    }
-                },
+                style: [ // the stylesheet for the graph
+                    {
+                        selector: 'node',
+                        style: {
+                            'background-color': '#666',
+                            'label': 'data(id)'
+                        }
+                    },
 
-                {
-                    selector: 'edge',
-                    style: {
-                        'width': 3,
-                        'line-color': '#ccc',
-                        'target-arrow-color': '#ccc',
-                        'target-arrow-shape': 'triangle',
-                        'curve-style': 'bezier'
+                    {
+                        selector: 'edge',
+                        style: {
+                            'width': 3,
+                            'line-color': '#ccc',
+                            'target-arrow-color': '#ccc',
+                            'target-arrow-shape': 'triangle',
+                            'curve-style': 'bezier'
+                        }
                     }
-                }
-            ],
+                ],
 
-            layout: {
+                layout: {
                     name: 'cose',
                     nodeRepulsion: node => {
                         if (node.hasClass('character')) {
@@ -109,19 +218,34 @@ export default defineComponent({
                         return 1000
                     }
                     // rows: 1
-            }
-        })
-
+                }
+            })
+            // cy.nodes()[0].popper({
+            //     content: () => {
+            //         var content = document.createElement('div');
+            //         content.innerHTML = `<p>${cy.nodes()[0].id()}</p>`;
+            //         return content;
+            //     },
+            //     popper: {}
+            // });
             cy.on('tap', 'node', function (evt) {
                 var node = evt.target;
                 if (node.hasClass('character')) {
                     console.log('tapped character' + node.id());
+                    // node.popper({
+                    //     content: () => {
+                    //         var content = document.createElement('div');
+                    //         content.innerHTML = `<p>${node.id()}</p>`;
+                    //         return content;
+                    //     },
+                    //     popper: {}
+                    // })
                 }
                 else if (node.hasClass('trope')) {
                     console.log('tapped trope ' + node.id());
                 }
             });
-        }
+        }*/
     },
     mounted() {
         console.log("Mounted character", this.character)
